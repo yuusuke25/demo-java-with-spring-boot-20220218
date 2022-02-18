d7ea419285658adb10d95337c4e632d5016142e2
Yuranan Charoen-ngarm <yuranan.c@kbtg.tech>
2022-02-18 15:23:39 +0700

    Add validation

---
+++ src/main/java/com/example/day02/demo/ValidationRule.java
@@ -0,0 +1,24 @@
+package com.example.day02.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+public interface ValidationRule {
+
+}
+
+@Component
+class EmailValidationV1 implements ValidationRule {
+
+}
+
+@Component
+class EmailValidationV2 implements ValidationRule {
+
+}
+
+@Component
+class DemoUseInterface {
+    @Autowired
+    private ValidationRule emailValidationV2;
+}
