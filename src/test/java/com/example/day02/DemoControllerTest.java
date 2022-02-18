2cdcc8ec48cf4e542e81f5603a9dcdea2874d805
Yuranan Charoen-ngarm <yuranan.c@kbtg.tech>
2022-02-18 16:54:35 +0700

    avoid runtime error to make test fail

--- src/test/java/com/example/day02/DemoControllerTest.java
+++ src/test/java/com/example/day02/DemoControllerTest.java
@@ -3,10 +3,12 @@ package com.example.day02;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class DemoControllerTest {
@@ -14,12 +16,17 @@ class DemoControllerTest {
     @Autowired
     TestRestTemplate restTemplate;
 
+    @MockBean
+    private DemoService demoService;
+
     @Test
     void xxxx() {
+        // Arrange
+        when(demoService.process(100)).thenReturn("Result with id = 100");
         // Act
-        DemoResponse result = restTemplate.getForObject("/demo/1",DemoResponse.class);
+        DemoResponse result = restTemplate.getForObject("/demo/100",DemoResponse.class);
         // Assert
-        assertEquals(0, result.getCode());
-        assertNull(result.getMessage());
+        assertEquals(100, result.getCode());
+        assertEquals("Result with id = 100",result.getMessage());
     }
 }
